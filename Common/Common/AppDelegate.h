56b9f83202dce412a439bb9bdd6d2f7d80664b16
summer.zhu <summer.zhu@vipshop.com>
2015-06-08 14:09:28 +0800

    firstCommit

---
+++ Common/Common/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Common
+//
+//  Created by summer.zhu on 8/6/15.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
